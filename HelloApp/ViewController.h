1fde697751e3e36095dab10ca7509569ee6f4637
Phillip <preed7@tigers.lsu.edu>
2012-07-31 14:25:11 -0500

    Designed UI

--- HelloApp/ViewController.h
+++ HelloApp/ViewController.h
@@ -10,4 +10,10 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
+@property (weak, nonatomic) IBOutlet UITextField *helloTextField;
+- (IBAction)helloButtonPressed:(UIButton *)sender;
+
+
+
 @end
