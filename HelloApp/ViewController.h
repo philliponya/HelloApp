66f03887101c6d7454550ddc4fabc85909f70ea6
Phillip <preed7@tigers.lsu.edu>
2012-07-31 13:38:41 -0500

    Initial Commit

---
+++ HelloApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloApp
+//
+//  Created by Phillip Reed on 7/31/12.
+//  Copyright (c) 2012 Phillip Reed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloApp
+//
+//  Created by Phillip Reed on 7/31/12.
+//  Copyright (c) 2012 Phillip Reed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
